d8b2756433c19e512fb65dc8f6da3c39a5d4fe1b
Royce <royce236@gmail.com>
2019-06-04 19:46:39 +0800

    Landing page

--- hris/employee/urls.py
+++ hris/employee/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 
+app_name = "employee"
+
 urlpatterns = [
-    
+    path("",Dashboard.as_view(),name="dashboard"),
 ]
--- hris/employee/views.py
+++ hris/employee/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+
+from main.models import HR
+from .models import Employee
 
 # Create your views here.
+class Dashboard(View):
+    def get(self, request, *args, **kwargs):
+        if "user_info" in request.session:
+            return render(request,template_name="employee/dashboard.html",context={})
+        else:
+            return redirect("/")
+
+    def post(self, request, *args, **kwargs):
+        pass
